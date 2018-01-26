2a310409824991987356db710257083c263e1686
HenryFBP <HenryFBP@gmail.com>
2018-01-26 11:51:32 -0600

    add library for general-purp funcs

---
+++ lab1/src/lab1/lib.java
@@ -0,0 +1,21 @@
+package lab1;
+
+public class lib
+{
+	/***
+	 * @param haystack The string to be searched for needle.
+	 * @param needle
+	 * @return If the String needle is in the String haystack.
+	 */
+	public static boolean contains(String haystack, String needle)
+	{
+		if(haystack.compareTo(needle) == 0)
+		{
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
+}
