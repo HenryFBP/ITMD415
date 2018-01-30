abd617e893a6f41fc2b0f0141d6c484284c748e2
Henry Post <HenryFBP@gmail.com>
2018-01-29 20:07:19 -0600

    move OIS and OOS to connect function

--- lab1/src/lab1/MathClient.java
+++ lab1/src/lab1/MathClient.java
@@ -9,95 +9,167 @@ import java.util.Scanner;
 
 public class MathClient
 {
-	private Socket socket;
-
+	private Socket socket = null;
+	private ObjectInputStream in = null;
+	private ObjectOutputStream out = null;
+	
+	public MathClient(String host, int port)
+	{
+		this.connect(host, port);
+	}
+	
+	public MathClient(Socket s)
+	{
+		this.socket = s;
+	}
+	
 	public void connect(String host, int port)
 	{
+		// if we already have a connection, don't connect
+		if(socket != null && !socket.isClosed())
+		{
+			return;
+		}
+
 		System.out.printf("Connecting to '%s:%d'\n", host, port);
 
 		try
 		{
 			socket = new Socket(host, port);
-
 			System.out.println("Connected to server!");
 
-			ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream()); // ObjectOutputStream to server
-			ObjectInputStream in = new ObjectInputStream(socket.getInputStream()); // ObjectInputStream from server
+			in = new ObjectInputStream(socket.getInputStream());
+			out = new ObjectOutputStream(socket.getOutputStream());
 
-			Scanner keyboard = new Scanner(System.in);
-			String input = "";
-
-			Message serverMsg = new Message();
-			Message clientMsg = new Message();
-
-			boolean keepgoing = true;
-
-			while(keepgoing == true)
-			{
-				try
-				{
-					serverMsg = (Message) in.readObject(); // read message from server
-
-					if(serverMsg.length() == 0) // if server wants to disconnect or we want to quit
-					{
-						System.out.println("Server wishes to terminate connection.");
-						keepgoing = false;
-						break;
-					}
-					else if(!HLib.shouldContinue(input.toUpperCase(), MathE.QUIT.s().toUpperCase()))
-					{
-						//prepare our quit signal
-						clientMsg = new Message();
-						out.writeObject(clientMsg);
-						out.flush();
-
-						System.out.println("Goodbye!");
-						keepgoing = false;
-						break;
-					}
-
-					serverMsg.toString(System.out); // print that message
-
-					// prepare message from keyboard
-					System.out.print(" > ");
-					clientMsg = new Message(input = keyboard.nextLine());
-
-					out.writeObject(clientMsg); // send message
-					out.flush();
-
-				}
-				catch(EOFException e)
-				{
-					System.out.println("Server's message terminated unexpectedly.");
-					e.printStackTrace();
-					return;
-				}
-
-			}
-
-			// close open streams
-			keyboard.close();
-			socket.close();
-			out.close();
-			in.close();
-
-			System.out.println("Connection terminated..."); // Signifies the end of the connection
-
+			System.out.println("I/O Streams established.");
 		}
 		catch(IOException e)
 		{
 			System.out.println("Client Error: " + e.getMessage());
 			e.printStackTrace();
 		}
+	}
+	
+	public void disconnect() throws IOException
+	{
+		socket.close();
+	}
+	
+	/***
+	 * Initiate a command-line REPL that interfaces with a math server.
+	 */
+	public void repl()
+	{
+		Scanner keyboard = new Scanner(System.in);
+
+		String input = "";
+
+		Message serverMsg = new Message();
+		Message clientMsg = new Message();
+
+		boolean keepgoing = true;
+
+		while(keepgoing == true)
+		{
+			try
+			{
+				serverMsg = (Message) in.readObject(); // read message from server
+
+				if(serverMsg.length() == 0) // if server wants to disconnect or we want to quit
+				{
+					System.out.println("Server wishes to terminate connection.");
+					keepgoing = false;
+					break;
+				}
+				else if(!HLib.shouldContinue(input.toUpperCase(), MathE.QUIT.s().toUpperCase()))
+				{
+					// prepare our quit signal
+					clientMsg = new Message();
+					out.writeObject(clientMsg);
+					out.flush();
+
+					System.out.println("Goodbye!");
+					keepgoing = false;
+					break;
+				}
+
+				serverMsg.toString(System.out); // print that message
+
+				// prepare message from keyboard
+				System.out.print(" > ");
+				clientMsg = new Message(input = keyboard.nextLine());
+
+				out.writeObject(clientMsg); // send message
+				out.flush();
+
+			}
+			catch(EOFException e)
+			{
+				System.out.println("Server's message terminated unexpectedly.");
+				e.printStackTrace();
+			}
+			catch(IOException e)
+			{
+				System.out.println("I/O Exception occurred.");
+				e.printStackTrace();
+			}
+			catch(ClassNotFoundException e)
+			{
+				e.printStackTrace();
+			}
+
+		}
+
+		// close open streams
+		keyboard.close();
+		
+		try
+		{
+			socket.close();
+		}
+		catch(IOException e)
+		{
+			System.out.println("Closing socket failed.");
+			e.printStackTrace();
+		}
+
+
+		System.out.println("Connection terminated."); // Signifies the end of the connection
+
+	}
+
+	/***
+	 * Has a client ask a server for the result of (opa op opb).
+	 * @param opa Operand a.
+	 * @param op  Operation op.
+	 * @param opb Operand b.
+	 * @return opa op opb
+	 * 
+	 * Example:
+	 * c(2.0, "**", 3.0) -> magic server math -> 8.0
+	 */
+	public double calculate(double opa, String op, double opb)
+	{
+		Message query = new Message(opa+op+opb);
+		Message response = null;
+		
+		try
+		{
+			out.writeObject(query); // send question
+			out.flush();
+			response = (Message) in.readObject(); // recieve response
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
 		catch(ClassNotFoundException e)
 		{
 			e.printStackTrace();
 		}
-	}
 
-	public int calculate(int opa, String op, int opb)
-	{
-		return -1;
+		
+		return (Double)response.result;
 	}
 
 	/***
@@ -107,8 +179,8 @@ public class MathClient
 	 */
 	public static void main(String[] args)
 	{
-		MathClient mathClient = new MathClient();
+		MathClient replMathClient = new MathClient(MathE.HOST.s(), MathE.PORT.i());
 
-		mathClient.connect(MathE.HOST.s(), MathE.PORT.i());
+		replMathClient.repl();
 	}
 }
--- lab1/src/lab1/MathServer.java
+++ lab1/src/lab1/MathServer.java
@@ -12,14 +12,26 @@ public class MathServer extends Thread
 {
 
 	protected Socket socket;
-	protected PrintStream log;
-
-	private MathServer(Socket socket, PrintStream o)
+	private ObjectOutputStream out;
+	private ObjectInputStream in;
+	
+	private MathServer(Socket socket)
 	{
 		this.socket = socket;
-		this.log = o;
-		log.printf("New Client: '%s'\n", this.toStringHP());
-		this.run();
+
+		try
+		{
+			out = new ObjectOutputStream(socket.getOutputStream());
+			in = new ObjectInputStream(socket.getInputStream());
+
+			System.out.printf("New Client: '%s'\n", this.toStringHP());
+			this.run();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+
 	}
 
 	/***
@@ -29,9 +41,6 @@ public class MathServer extends Thread
 	{
 		try
 		{
-			ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-			ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-
 			Message serverMsg = new Message();
 			Message clientMsg = new Message();
 			Statement s = null;
@@ -40,27 +49,27 @@ public class MathServer extends Thread
 			serverMsg.add(String.format("Enter (%s) to quit.", MathE.QUIT.s()));
 			serverMsg.add(String.format("Valid operators: %s",
 					Arrays.toString(MathE.VALID_OPS).replaceAll("[\\[\\]\\\"]", "")));
-			
+
 			// send object initially
 			out.writeObject(serverMsg);
 			out.flush();
-			
-			this.log.println("Wrote beginning message.");
+
+			System.out.printf("[%s]: Wrote beginning message.\n", this.toStringHP());
 
 			boolean keep = true;
-			
+
 			// while we should keep going
 			while(keep == true)
 			{
-				s = null; //set math statement to null
-				serverMsg = null; //set out response to null
+				s = null; // set math statement to null
+				serverMsg = null; // set out response to null
 
-				clientMsg = (Message) in.readObject(); //get response
-				
-				if(clientMsg.length() <= 0) //if they want to quit
+				clientMsg = (Message) in.readObject(); // get response
+
+				if(clientMsg.length() <= 0) // if they want to quit
 				{
 					keep = false;
-					System.out.printf("[%s]: Wants to quit.", this.toStringHP());
+					System.out.printf("[%s]: Wants to quit.\n", this.toStringHP());
 					break;
 				}
 				
@@ -80,11 +89,14 @@ public class MathServer extends Thread
 				
 				if(s != null) //if math was successfully parsed
 				{
-					serverMsg = new Message(String.format("'%s' = %.2f",clientMsg.flatten(), s.result()));
+					serverMsg = new Message(String.format("'%s' = %.2f",clientMsg.flatten(), s.result())); // add formatted result
+					serverMsg.result = s.result(); // add actual number result
+					
 					System.out.print(s.result());
 				}
 				
 				out.writeObject(serverMsg); //output whatever our message is
+				out.flush();
 				
 
 				System.out.print("\n");
@@ -95,7 +107,7 @@ public class MathServer extends Thread
 		catch(IOException e)
 		{
 			e.printStackTrace();
-			this.log.printf("Error reading/writing for client %s!\n", this.toStringHP());
+			System.out.printf("Error reading/writing for client %s!\n", this.toStringHP());
 		}
 		catch(ClassNotFoundException e)
 		{
@@ -130,7 +142,7 @@ public class MathServer extends Thread
 			while(true) // blocking accept request FOREVER!!! :)
 			{
 				System.out.println("Waiting for client to connect...");
-				new MathServer(server.accept(), System.out);
+				new MathServer(server.accept());
 			}
 		}
 		catch(IOException ioe)
