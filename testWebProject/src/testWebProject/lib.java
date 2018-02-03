454efc8f97e8700b34da693677c591c4dcbe73b7
Henry Post <HenryFBP@gmail.com>
2018-02-02 18:53:09 -0600

    add fileToString helperfn

--- testWebProject/src/testWebProject/lib.java
+++ testWebProject/src/testWebProject/lib.java
@@ -1,11 +1,57 @@
 package testWebProject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Random;
+import java.util.Scanner;
+
+import javax.servlet.ServletContext;
 
 public class lib
 {
-	 public static double randomNumber()
-	 {
-	   return (new Random()).nextDouble() * 100;
-	 }
+	public static double randomNumber()
+	{
+		return (new Random()).nextDouble() * 100;
+	}
+
+	public static String fileToString(String s)
+	{
+		return fileToString(new File(s));
+	}
+
+	public static String fileToString(File f)
+	{
+		Scanner s = null;
+		String line = "";
+		String lines = "";
+
+		try
+		{
+			s = new Scanner(f);
+		}
+		catch(FileNotFoundException e)
+		{
+			e.printStackTrace();
+			return "";
+		}
+
+		while(s.hasNext())
+		{
+			line = s.nextLine();
+			lines += line;
+		}
+
+		s.close();
+
+		return lines;
+	}
+
+	/***
+	 * @return A navbar HTML element.
+	 */
+	public static String navbar(ServletContext sc)
+	{
+		return fileToString(sc.getRealPath(sc.getContextPath()) + "testWebProject/navbar.inc");
+	}
+
 }
