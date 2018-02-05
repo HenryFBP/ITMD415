47daa800119f6f1b361acf9de0311c3eaad7f490
HenryFBP <HenryFBP@gmail.com>
2018-02-04 20:41:37 -0600

    make doGet() more personal and verbose for CommentControllerServlet

--- testWebProject/src/testWebProject/Comment/CommentControllerServlet.java
+++ testWebProject/src/testWebProject/Comment/CommentControllerServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import testWebProject.lib;
+
 /**
  * Servlet implementation class CommentControllerServlet
  */
@@ -31,7 +33,10 @@ public class CommentControllerServlet extends HttpServlet
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append(lib.wrap(String.format("'%s' aka Comment Controller Servlet FTW!",this.getClass().getName()),"p"));
+		response.getWriter().append(lib.wrap(String.format("Served at: %s",request.getContextPath()),"p"));
+		
+		
 	}
 
 	/**
