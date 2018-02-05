693580190ecbafe7272f7c72f9bbf2af197f31af
HenryFBP <HenryFBP@gmail.com>
2018-02-04 21:23:45 -0600

    forward GET request to jsp page but also allow intercepting it

--- testWebProject/src/testWebProject/Comment/CommentControllerServlet.java
+++ testWebProject/src/testWebProject/Comment/CommentControllerServlet.java
@@ -15,39 +15,21 @@ import testWebProject.lib;
 @WebServlet("/CommentControllerServlet")
 public class CommentControllerServlet extends HttpServlet
 {
-	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public CommentControllerServlet()
-	{
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-		// TODO Auto-generated method stub
-		response.getWriter().append(lib.wrap(String.format("'%s' aka Comment Controller Servlet FTW!",this.getClass().getName()),"p"));
-		response.getWriter().append(lib.wrap(String.format("Served at: %s",request.getContextPath()),"p"));
+		//response.getWriter().append(lib.wrap(String.format("'%s' aka Comment Controller Servlet FTW!",this.getClass().getName()),"p"));
+		//response.getWriter().append(lib.wrap(String.format("Served at: %s",request.getContextPath()),"p"));
 		
+		log("Someone is GETing our CommentControllerServlet");
 		
+		request.getRequestDispatcher("/tests/comment/index.jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
 		log("DID SUM1 TRY TO (HTTP POST) A COMMENT????!?!?!");
-		// TODO Auto-generated method stub
-		doGet(request, response);
 	}
 
 }
