b860e30beefad2bcbf5a4329dd20103d8a15767e
Henry Post <HenryFBP@gmail.com>
2018-02-05 15:11:21 -0600

    add CREATE TABLE for comment

--- testWebProject/src/resources/queries.sql
+++ testWebProject/src/resources/queries.sql
@@ -1,2 +1,16 @@
-CREATE DATABASE testdb;
+--USING testdb;
 
+CREATE DATABASE IF NOT EXISTS testdb;
+
+
+DROP TABLE comment;
+
+CREATE TABLE IF NOT EXISTS comment (
+   comment_id 		INT 		NOT NULL auto_increment,
+   name 			VARCHAR(64) NOT NULL,
+   content			TEXT 		NOT NULL,
+   date     		DATE 		NOT NULL,
+   PRIMARY KEY (comment_id)
+);
+
+DESCRIBE comment;
