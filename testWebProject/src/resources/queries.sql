132655a75c40749fd3c2670a4f448eff4bb34f93
Henry Post <HenryFBP@gmail.com>
2018-02-05 21:01:17 -0600

    sql schema DATE->BIGINT

--- testWebProject/src/resources/queries.sql
+++ testWebProject/src/resources/queries.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS comment (
    comment_id 		INT 		NOT NULL AUTO_INCREMENT,
    name 			VARCHAR(64) NOT NULL,
    content			TEXT 		NOT NULL,
-   date     		DATE 		NOT NULL,
+   date     		BIGINT 		NOT NULL,
    PRIMARY KEY (comment_id)
 );
 
