0f2cb51c572aaeee4ccc4ed3a83ecb84d1c1f15d
HenryFBP <HenryFBP@gmail.com>
2018-02-08 19:22:35 -0600

    add CREATE TABLE for user table

--- testWebProject/src/resources/queries.sql
+++ testWebProject/src/resources/queries.sql
@@ -2,14 +2,29 @@ CREATE DATABASE IF NOT EXISTS testdb;
 
 USE testdb;
 
-DROP TABLE IF EXISTS comment;
+--DROP TABLE IF EXISTS comment;
 CREATE TABLE IF NOT EXISTS comment (
    comment_id 		INT 		NOT NULL AUTO_INCREMENT,
    name 			VARCHAR(64) NOT NULL,
    content			TEXT 		NOT NULL,
    date     		BIGINT 		NOT NULL,
+
    PRIMARY KEY (comment_id)
 );
 
 DESCRIBE comment;
 SELECT * FROM comment;
+
+--DROP TABLE IF EXISTS user;
+CREATE TABLE IF NOT EXISTS user (
+	user_id			INT			NOT NULL AUTO_INCREMENT,
+	name			VARCHAR(64) NOT NULL,
+	email			VARCHAR(64) NOT NULL,
+	password		VARCHAR(64) NOT NULL,
+	dob				BIGINT		NOT NULL,
+	
+	PRIMARY KEY (user_id)
+);
+
+DESCRIBE user
+SELECT * FROM user;
