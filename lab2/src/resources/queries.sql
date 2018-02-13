8c516e1a71f3a0c1c42c0e40b3afaba69a66c602
Dennis Chase <dchase3@hawk.iit.edu>
2018-02-13 17:37:45 -0600

    Fixed auto increment to unique

--- lab2/src/resources/queries.sql
+++ lab2/src/resources/queries.sql
@@ -6,7 +6,7 @@ CREATE DATABASE IF NOT EXISTS testdb;
 DROP TABLE comment;
 
 CREATE TABLE IF NOT EXISTS comment (
-   comment_id 		INT 		NOT NULL AUTO_INCREMENT,
+   comment_id 		INT 		NOT NULL UNIQUE AUTO_INCREMENT,
    name 			VARCHAR(64) NOT NULL,
    content			TEXT 		NOT NULL,
    date     		BIGINT 		NOT NULL,
