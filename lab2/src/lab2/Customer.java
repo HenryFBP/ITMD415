512d74d0a649f552eeca7afa7bb2b68e6ff656c6
HenryFBP <HenryFBP@gmail.com>
2018-02-19 14:57:24 -0600

    finish Customer object

--- lab2/src/lab2/Customer.java
+++ lab2/src/lab2/Customer.java
@@ -2,12 +2,92 @@ package lab2;
 
 public class Customer
 {
-    public String name;
-    public String SSN;
-    
-    public Customer(String n, String ssn)
-    {
-        this.name = n;
-        this.SSN = ssn;
-    }
+	private String name;
+	private String SSN;
+	private String zip;
+	private String email;
+	private String street;
+	private String city;
+	private String state;
+
+	public Customer(String name, String ssn, String zip, String email, String street, String city, String state)
+	{
+		this.name = name;
+		this.SSN = ssn;
+		this.zip = zip;
+		this.email = email;
+		this.street = street;
+		this.city = city;
+		this.state = state;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getSSN()
+	{
+		return SSN;
+	}
+
+	public void setSSN(String sSN)
+	{
+		SSN = sSN;
+	}
+
+	public String getZip()
+	{
+		return zip;
+	}
+
+	public void setZip(String zip)
+	{
+		this.zip = zip;
+	}
+
+	public String getEmail()
+	{
+		return email;
+	}
+
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+
+	public String getStreet()
+	{
+		return street;
+	}
+
+	public void setStreet(String street)
+	{
+		this.street = street;
+	}
+
+	public String getCity()
+	{
+		return city;
+	}
+
+	public void setCity(String city)
+	{
+		this.city = city;
+	}
+
+	public String getState()
+	{
+		return state;
+	}
+
+	public void setState(String state)
+	{
+		this.state = state;
+	}
 }
