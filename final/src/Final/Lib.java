cf97671529fba75bdb93543c5ba5b50d4f2b9049
HenryFBP <HenryFBP@gmail.com>
2018-03-06 15:50:37 -0600

    add skeleton of hashing function

---
+++ final/src/Final/Lib.java
@@ -0,0 +1,14 @@
+package Final;
+
+public class Lib
+{
+    /***
+     * Hash a String to a 512-bit (64-byte) bitstring.
+     * @param s
+     * @return
+     */
+    public Byte[] hashString(String s)
+    {
+        return null;
+    }
+}
