95e706a5015e5bddc344433385642289b5a91ff8
HenryFBP <HenryFBP@gmail.com>
2018-03-06 18:33:11 -0600

    extend passhash length to work with jhash's length of hashes

--- final/src/resources/customer.sql
+++ final/src/resources/customer.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS customer (
     cid         INTEGER         NOT NULL UNIQUE AUTO_INCREMENT,
     username    VARCHAR(64)     NOT NULL UNIQUE,
     email       VARCHAR(64)     NOT NULL UNIQUE,
-    passhash    VARCHAR(64)     NOT NULL,
+    passhash    VARCHAR(128)    NOT NULL,
     
     PRIMARY KEY (cid)
 );
