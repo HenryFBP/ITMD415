43df9c61267ddad5909619ba53c77f3efc68a616
Henry Post <HenryFBP@gmail.com>
2018-03-07 22:05:06 -0600

    remove redundant SELECT query

--- final/src/resources/data.sql
+++ final/src/resources/data.sql
@@ -4,7 +4,6 @@ INSERT INTO customer VALUES (1, "HenryFBP", "HenryFBP@gmail.com", "pbkdf2sha1:64
 INSERT INTO customer VALUES (2, "dchase", "dchase3@hawk.iit.edu", "pbkdf2sha1:64000:18:24:n:hh82TcSHudimd8whGG3DdJ7wegN6PcqH:CkoMEIu82FjAm35I1zmWG0q7"); -- youmayguess
 INSERT INTO customer VALUES (3, "AdmiralAckbar", "Ackbar@gmail.com", "pbkdf2sha1:64000:18:24:n:Hy4GIW3CjqvlL3yW35aeQ3BbTZgvL6ZE:pe8rQHdVr43DiXNWzhP1Lkbw"); -- itsatrap
 INSERT INTO customer VALUES (4, "God", "BigG@heaven.gov", "pbkdf2sha1:64000:18:24:n:jb5TMaeqwhLi7J66jBDvs3DuWQ2Wd1+T:HDA6gyEwFDJGo+8G6lKI97je"); -- trueadmin
-SELECT * FROM customer;
 
 INSERT INTO product VALUES (1, 2, 1, "Engine", 150);
 INSERT INTO product VALUES (2, 1, 2, "Lexus", 50000);
