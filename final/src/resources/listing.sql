5cca4da7309274b375e43fafbf1d99e91c5897ef
HenryFBP <HenryFBP@gmail.com>
2018-03-22 16:50:12 -0500

    remove unnecessary relations from `listing`

--- final/src/resources/listing.sql
+++ final/src/resources/listing.sql
@@ -5,15 +5,12 @@ USE itmd415;
 CREATE TABLE IF NOT EXISTS listing (
     lid         INTEGER         NOT NULL UNIQUE AUTO_INCREMENT,
     pid         INTEGER         NOT NULL,
-    sellercid   INTEGER         NOT NULL,
     buyercid    INTEGER         ,
     fid         INTEGER         ,
     
     PRIMARY KEY (lid),
     FOREIGN KEY (pid)           REFERENCES product(pid),
-    FOREIGN KEY (sellercid)     REFERENCES customer(cid),
     FOREIGN KEY (buyercid)      REFERENCES customer(cid),
-    FOREIGN KEY (fid)           REFERENCES forum(fid)
     
 );
 
