658a6a4d3ceb2f400d0f56f98439e907cf0c663a
HenryFBP <HenryFBP@gmail.com>
2018-03-25 19:36:04 -0500

    create stub servlets

---
+++ final/src/Final/Product/PartCreationServlet.java
@@ -0,0 +1,16 @@
+package Final.Product;
+
+import javax.servlet.http.HttpServletRequest;
+
+import Final.Product.Part.Part;
+
+public class PartCreationServlet
+{
+
+    public static Part createPart(HttpServletRequest r)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
