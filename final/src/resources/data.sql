ebfb0de3f9d7901d43c5dad7e275ef6a704237a7
Henry Post <HenryFBP@gmail.com>
2018-03-27 15:44:31 -0500

    add example car product for 'a'

--- final/src/resources/data.sql
+++ final/src/resources/data.sql
@@ -15,6 +15,7 @@ INSERT INTO car VALUES (1, "Ford", "Fiesta", 10000, 1997); -- henry's ford fiest
 INSERT INTO car VALUES (2, "Lexus", "CT", 20000, 1994); -- dennis' lexus CT
 INSERT INTO car VALUES (3, "Godco", "Godmobile", 90000, 0); -- god's car
 INSERT INTO car VALUES (4, "Resistance", "Tie Fighter", 900000, 2020); -- ackbar's tie fighter
+INSERT INTO car VALUES (5, "Generico", "Genericar", 100, 2000) --a's generic car
 
 INSERT INTO part VALUES (1, "Ford", "Fiesta", "Tire", "New"); -- henry's ford fiesta tire
 INSERT INTO part VALUES (2, "Ford", "Fiesta", "Engine", "Good"); -- henry's ford fiesta engine
@@ -26,10 +27,11 @@ INSERT INTO product VALUES (1,    1,      1,      1  ,  NULL,  "New Ford Fiesta"
 INSERT INTO product VALUES (2,    1,      2,      2  ,  NULL,  "Good Lexus CT");
 INSERT INTO product VALUES (3,    1,      4,      3  ,  NULL,  "God's Car");
 INSERT INTO product VALUES (4,    1,      3,      4  ,  NULL,  "Tie Fighter");
-INSERT INTO product VALUES (5,    2,      1,     NULL,   1  ,  "New Ford Fiesta Tire");
-INSERT INTO product VALUES (6,    2,      1,     NULL,   2  ,  "Good Ford Fiesta Engine");
-INSERT INTO product VALUES (7,    2,      4,     NULL,   3  ,  "Fresh Jesus Juice");
-INSERT INTO product VALUES (8,    2,      3,     NULL,   4  ,  "OK Tie Blaster");
+INSERT INTO product VALUES (5,    1,      6,      5  ,  NULL,  "Genericar v1.0");
+INSERT INTO product VALUES (6,    2,      1,     NULL,   1  ,  "New Ford Fiesta Tire");
+INSERT INTO product VALUES (7,    2,      1,     NULL,   2  ,  "Good Ford Fiesta Engine");
+INSERT INTO product VALUES (8,    2,      4,     NULL,   3  ,  "Fresh Jesus Juice");
+INSERT INTO product VALUES (9,    2,      3,     NULL,   4  ,  "OK Tie Blaster");
 
 --                          pk | product id | buyer | price |
 INSERT INTO listing VALUES (1,       1,       NULL,   20000);
