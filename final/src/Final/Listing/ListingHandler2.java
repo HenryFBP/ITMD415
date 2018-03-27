a49ad8c571e8243c63a2bc6aef4ecf0c351eef04
Henry Post <HenryFBP@gmail.com>
2018-03-26 19:05:42 -0500

    add skel Listing->HTML handler

---
+++ final/src/Final/Listing/ListingHandler2.java
@@ -0,0 +1,14 @@
+package Final.Listing;
+
+public class ListingHandler2
+{
+    /***
+     * Given a Listing, return an HTML element representing that Listing.
+     */
+    public static String listingToHTML(Listing l)
+    {
+        String ret = "";
+        
+        return ret;
+    }
+}
